48530baff17a88de88f9d786243a7e79ba63dced
keyth202 <combs.cdi@gmail.com>
2017-03-16 21:57:43 -0400

    still working on it

--- app.js
+++ app.js
@@ -1,107 +1,57 @@
 
-var quizItems =[
-{
-	question: "What is Magneto's real name?",
-	cAns1: "Max Eisenhardt",
-	fAns1: "Erik Lensherrer",
-	fAns2: "Kurt Wagonner",
-},
-{
-	question: "Where was Storm born?",
-	cAns1: "Manhattan",
-	fAns1: "Cairo",
-	fAns2: "Wakanda",
-},
-{
-	question: "Who is Kurt Wagoner/Nightcrawler's mother?",
-	cAns1: "Mystique",
-	fAns1: "Jean Grey",
-	fAns2: "Azazel",
-},
-{
-	question: "What is Wolverine's real name?",
-	cAns1: "James Howlett",
-	fAns1: "Logan",
-	fAns2: "Victor Creed",
-},
-{
-	question: "What superhero did Rogue kill to gain the ability to fly?",
-	cAns1: "Ms Marvel",
-	fAns1: "Angel",
-	fAns2: "Jean Grey",
-},
-{
-	question: "Which alternate timeline gay mutant was thrown into hell for kissing a demigod?",
-	cAns1: "Wolverine",
-	fAns1: "Colussus",
-	fAns2: "Iceman",
-},
-{
-	question: "Which is the first openly gay mutant in the marvel universe?",
-	cAns1: "Northstar",
-	fAns1: "Bobby Drake",
-	fAns2: "Wiccan",
-},
-{
-	question: "The Weapon X program was based on which mutant's Nazi experiments?",
-	cAns1: "Nathan Essex/ Mr. Sinister ",
-	fAns1: "Redskull",
-	fAns2: "Dr Octopus",
-},
-{
-	question: "Which character has not been an X-men, Avenger and villain?",
-	cAns1: "Hank Pym",
-	fAns1: "Deadpool",
-	fAns2: "Wanda Maximoff",
-}];
-
-console.log(quizItems[1].question);
-
-
-
-
-function quizapp(){
-	var correctScore =0; 
-	var incorrectScore =0;
-
-	for(var clickCount =0; clickCount <3; clickCount++){
-//Function to generate list of questions and place them in an html field
-		for(var i=0; i<3; i++){
-			var ranNum = Math.floor(Math.random()*10);
-			var htmlQues= '<span class="question-item"><p>'+quizItems[ranNum].question+'</p>';
-			var htmlcAns= '<label><input type="radio" name="ans1" value="true" />'+quizItems[ranNum].cAns1+'</label></br>';
-			var htmlfAns1='<label><input type="radio" name="ans1" value="false" />'+quizItems[ranNum].fAns1+'</label></br>';
-			var htmlfAns2='<label><input type="radio" name="ans1" value="false" />'+quizItems[ranNum].fAns2+'</label></br></span>';
-			if(i%2 === 0){
-				$('.question-list').html(htmlQues+htmlcAns+htmlfAns1+htmlfAns2);
-			}
-			else{
-				$('.question-list').html(htmlQues+htmlfAns2+htmlfAns1+htmlcAns);
-			};
-		}
-
-//function to count correct and incorrect answers and place them in bottom html 
-		clickCount = $('.submit-answer').on('click', this, function(e){
-			e.preventDefault();
-			var choice = document.getElementById("js-quiz").value; 
-			if(choice == 'true'){
-				correctScore++;
-				$('.js-correct-score-counter').text(correctScore);
-			}
-			else{
-				incorrectScore++;
-				$('.js-incorrect-score-counter').text(inccorrectScore);
-
-			}
-			
-		});
-	}
+var state ={
+	quiz: [
+				{
+					question: "What is Magneto's real name?",
+					answer:[ "Max Eisenhardt","Erik Lensherrer","Kurt Wagonner", "Henrich Von Heim"],
+					corPos: 0,
+				},
+				{
+					question: "Where was Storm born?",
+					answer: ["Cairo","Wakanda", "Manhattan","Tanzania"],
+					corPos:2,
+				},
+				{
+					question: "Who is Kurt Wagoner/Nightcrawler's mother?",
+					answer: ["Jean Grey","Azazel","Mystique","Jubilee"],
+					corPos:2,
+				},
+				{
+					question: "What is Wolverine's real name?",
+					answer: ["James Howlett","Logan","Victor Creed","Sebastian Stan"],
+					corPos: 0,
+				},
+				{
+					question: "What superhero did Rogue kill to gain the ability to fly?",
+					answer: ["Angel","Jean Grey","Ms Marvel","Northstar"],
+					corPos: 2,
+				},
+				{
+					question: "Which alternate timeline gay mutant was thrown into hell for kissing a demigod?",
+					answer: ["Colussus","Wolverine","Iceman","Midnighter"],
+					corPos: 1,
+				},
+				{
+					question: "Which is the first openly gay mutant in the marvel universe?",
+					answer: ["Bobby Drake","Wiccan","Hulkling","Northstar"],
+					corPos: 3,
+				},
+				{
+					question: "The Weapon X program was based on which mutant's Nazi experiments?",
+					answer: ["Nathan Essex/ Mr. Sinister ","Redskull","Dr Octopus","Moira McTaggert"],
+					corPos: 0,
+				},
+				{
+					question: "Which character has not been an X-men, Avenger and villain?",
+					answer:["Deadpool","Hank Pym","Wanda Maximoff","Quicksilver"],
+					corPos: 0,
+				}],
+	correctScore: 0,
+	currentPosition: 0
 };
 
 
-
-
-
+var guess = state.currentScore;
 
 $(document).ready(function(){
 	$('#js-quiz-start').submit(function(e){
@@ -109,6 +59,79 @@ $(document).ready(function(){
 		$('.question-list').removeClass('hidden');
 		$('.submit-answer').removeClass('hidden');
 		$('#js-quiz-start').addClass('hidden');
-		quizapp()
+		displayQuestions();
+		checkAnswer();
+		console.log(state.currentPosition);
+		endGame();
 	});
 });
+
+
+// randomly choose 3 questions and push them to chosenQuestions;
+// generate a random number, push that question, then splice that question from quizItems
+// generate random number based on quizItems.length not hard code in 10
+
+
+function displayQuestions(){
+	var ques = state.quiz[state.currentPosition];
+	console.log(state.currentPosition);
+	if(state.currentPosition == state.quiz.length){
+		displayResults();
+	}
+	else
+		$('.question-item').html('<p>'+ques.question+'</p>');
+		console.log(state.quiz[state.currentPosition]);
+
+		
+
+		for( var i = 0; i < ques.answer.length; i++){
+			if (i != ques.corPos){
+				$('.question-item').append('<label><input type="radio" name="ans1" value="0" />'+ques.answer[i]+'</label></br>');
+			}
+			else{
+				$('.question-item').append('<label><input type="radio" name="ans1" value="1" />'+ques.answer[i]+'</label></br>');
+			}
+			
+
+		}
+}
+ function checkAnswer(){
+ 	$('.submit-answer').on('click', this, function(e){
+ 		e.preventDefault();
+ 		var result= $('input:radio[name="ans1"]:checked');
+ 		console.log(result.val());
+ 		if( result.val() == 1){
+ 			state.correctScore++
+ 		};
+ 		state.currentPosition++ ; 
+ 		console.log(state.correctScore);
+ 		console.log(state.currentPosition)
+ 	
+ 		displayQuestions();
+ 	});
+ 	
+}
+
+
+function displayResults(){
+	if( state.currentPosition == state.quiz.length){
+		$('.js-score').removeClass('hidden');
+		$('.js-score').text('You Scored '+state.correctScore+' out of '+ state.quiz.length);
+		$('.js-end').removeClass('hidden');
+	}
+	endGame();
+}
+
+function endGame(){
+	
+	$('.submit-answer').on('click', this, function(e){
+ 		e.preventDefault();
+ 		state.currentPosition =0;
+ 		state.correctScore =0;
+ 		displayQuestions();
+	});
+}
+
+
+
+
